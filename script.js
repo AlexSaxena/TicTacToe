9c54fac3eb27e937af4f37585fa38e5f4b3b7ce7
AlexSaxena <saxenaalex@gmail.com>
2022-05-19 15:07:54 +0200

    Adds Board Module | Player Factory

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 console.log("Greeting General Kenobi!");
+
+// Gameboard Array Module
+const Gameboard = (function () {
+  let gameboard = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+  ];
+  return gameboard;
+})();
+
+// Player Factory
+const Player = (name = "New Player", marker = "X") => {
+  let playerName = name;
+  let playerMarker = marker;
+  return { name: playerName, marker: playerMarker };
+};
